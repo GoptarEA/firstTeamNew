259ccf5d10faba6b3951f297835883dc7920c51d
GoptarEA <goptarea@gmail.com>
2023-01-22 16:42:42 +0300

    следующая версия

--- flsite.py
+++ flsite.py
@@ -1,17 +1,25 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import (
+    Flask,
+    render_template,
+    request,
+    redirect,
+    url_for,
+    flash)
+
 from flask_sqlalchemy import SQLAlchemy
+
 from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)  #создание веб-приложения
 
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+app.secret_key = 'thisismysecretkey'
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 
 db = SQLAlchemy(app)
 
-authtorise = False
+
 
 
 class Users(db.Model):
@@ -36,52 +44,98 @@ class Routes(db.Model):
 
 
 @app.route("/")
+@app.route("/profile")
 def index():
     return render_template('index.html', title="Основная страница")
 
-@app.route("/main_page/<check>&<valuelogin>")
-def main_page(check, valuelogin):
-    print(check)
-    print(valuelogin)
-    return render_template('index.html', title="Основная страница", checklogin=check, login=valuelogin)
 
 
-@app.route("/db_error")
-def db_error():
-    return '''
-    <h1><center>Произошла ошибка при регистрации нового пользователя. 
-    Не расстраивайтесь и попробуйте зарегистрироваться ещё раз позже.</center></h1>
-    '''
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # проверка логика и пароля
+        return redirect(url_for('profile'))
+    flash(message="Invalid username or password.", category="message")
+    return render_template('login.html')
 
-@app.route("/registration", methods=['post', 'get'])
+
+@app.route("/registration", methods=['GET', 'POST'])
 def registration():
     if request.method == 'POST':
-        res = Users.query.all()
-        checked = True
-        for person in res:
-            if person.email == request.form.get('email'):
-                flash(message="Пользователь с таким логином уже существует", category="message")
-                checked = False
+        # Добавление пользователя в базу данных
+        pass
 
-        if checked:
-            try:
-                hash = generate_password_hash(request.form.get('password'))
-                u = Users(email=request.form.get('email'), password=hash, login=request.form.get('email'))
-                db.session.add(u)
-                db.session.flush()
 
-                # p = Profiles(login=request.form.get('email'), email=request.form.get('email'),
-                #              id=u.id)
-                # db.session.add(p)
-                db.session.commit()
-            except:
-                db.session.rollback()
-                return redirect(url_for("db_error"))
-            return redirect(url_for('main_page', check=True, valuelogin=request.form.get('email')))
+@app.route('/change_password', methods=['GET', 'POST'])
+def change_password():
+    #Страница с восстановлением пароля
+    pass
 
-        # login = request.form.get('email')
-        # print(login)
-    return render_template('registration.html', title="Регистрация")
+
+@app.route('/password_send', methods=['GET', 'POST'])
+def password_send():
+    # Страница с сообщением что новый пароль отправлен на почту
+    return render_template('password_send.html')
+
+@app.route('/delete_account', methods=['GET', 'POST'])
+def delete_account():
+    # Страница с удалением аккаунта
+    return render_template('delete_account.html')
+
+
+
+
+
+
+
+
+
+
+
+#
+# @app.route("/main_page/<check>&<valuelogin>")
+# def main_page(check, valuelogin):
+#     print(check)
+#     print(valuelogin)
+#     return render_template('index.html', title="Основная страница", checklogin=check, login=valuelogin)
+#
+#
+# @app.route("/db_error")
+# def db_error():
+#     return '''
+#     <h1><center>Произошла ошибка при регистрации нового пользователя.
+#     Не расстраивайтесь и попробуйте зарегистрироваться ещё раз позже.</center></h1>
+#     '''
+#
+# @app.route("/registration", methods=['post', 'get'])
+# def registration():
+#     if request.method == 'POST':
+#         res = Users.query.all()
+#         checked = True
+#         for person in res:
+#             if person.email == request.form.get('email'):
+#                 flash(message="Пользователь с таким логином уже существует", category="message")
+#                 checked = False
+#
+#         if checked:
+#             try:
+#                 hash = generate_password_hash(request.form.get('password'))
+#                 u = Users(email=request.form.get('email'), password=hash, login=request.form.get('email'))
+#                 db.session.add(u)
+#                 db.session.flush()
+#
+#                 # p = Profiles(login=request.form.get('email'), email=request.form.get('email'),
+#                 #              id=u.id)
+#                 # db.session.add(p)
+#                 db.session.commit()
+#             except:
+#                 db.session.rollback()
+#                 return redirect(url_for("db_error"))
+#             return redirect(url_for('main_page', check=True, valuelogin=request.form.get('email')))
+#
+#         # login = request.form.get('email')
+#         # print(login)
+#     return render_template('registration.html', title="Регистрация")
 
 
 if __name__ == "__main__":
